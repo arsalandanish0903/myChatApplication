a0bd1a5707d8d5dfd2c3b795091e5f09c5b9b7ad
Arsalan Danish <arsalandanish0903@gmail.com>
2025-04-04 18:29:21 +0530

    update name

--- client/src/pages/home/UserSidebar.jsx
+++ client/src/pages/home/UserSidebar.jsx
@@ -42,8 +42,8 @@ const UserSidebar = () => {
 
   return (
     <div className="max-w-[20em] w-full h-screen flex flex-col border-r border-r-white/10">
-      <h1 className="bg-black mx-3 rounded-lg mt-3 px-2 py-1 text-[#7480FF] text-xl font-semibold">
-        GUP SHUP
+      <h1 className="bg-black mx-3 rounded-lg mt-3 px-2 py-1 text-[#7480FF] text-2xl font-semibold">
+      TalkZone
       </h1>
 
       <div className="p-3">
